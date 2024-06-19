b5bfda702a233efb37f68303a221768634080ad0
lpmelo <lpmelo10@gmail.com>
2024-06-19 00:34:18 -0300

    feat: Implementing react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { globalStore } from "./redux/store.jsx";
 import { ThemeProvider } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
 import defaultTheme from "./config/theme/theme.jsx";
-import App from "./app/App.jsx";
+import router from "./routes/router.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={globalStore}>
       <ThemeProvider theme={defaultTheme}>
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
