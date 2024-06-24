c2a97dbaf1ed9dd2457e9b4ec9264e247df70839
lpmelo <lpmelo10@gmail.com>
2024-06-24 10:05:43 -0300

    feat: Implementing environment and mock data

--- src/features/challengeApi/challengeApi.jsx
+++ src/features/challengeApi/challengeApi.jsx
@@ -1,20 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import axios from "axios";
+import { companyData, menuData } from "./mock";
+
+const ambient = import.meta.env.VITE_AMBIENT;
+const baseUrl = ambient === "development" ? "" : import.meta.env.VITE_BASE_URL;
 
 export const challengeApi = createApi({
   reducerPath: "challengeApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://cdn-dev.preoday.com/challenge",
-  }),
+  baseQuery: fetchBaseQuery(),
   endpoints: (builder) => ({
     getCompanyById: builder.query({
       queryFn: async (id) => {
         try {
-          const response = await axios.get(`venue/${id}`);
+          const response = await axios.get(`${baseUrl}/venue/${id}`);
 
           return { data: response.data };
         } catch (error) {
-          return { error };
+          return { data: companyData };
         }
       },
     }),
@@ -22,10 +24,10 @@ export const challengeApi = createApi({
     getAllMenus: builder.query({
       queryFn: async () => {
         try {
-          const response = await axios.get("/menu");
+          const response = await axios.get(`${baseUrl}/menu`);
           return { data: response.data };
         } catch (error) {
-          return { error };
+          return { data: menuData };
         }
       },
     }),
